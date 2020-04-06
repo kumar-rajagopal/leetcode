9e4d09901bc0f3478ad6f4a562aa71a4c674fe64
kumar-rajagopal <62855730+kumar-rajagopal@users.noreply.github.com>
2020-04-05 19:26:51 -0700

    Update word_ladder.py

--- linkedin/tree_and_graphs/word_ladder.py
+++ linkedin/tree_and_graphs/word_ladder.py
@@ -35,3 +35,29 @@ Output: 0
 
 Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
 """
+
+from collections import defaultdict, deque
+
+
+def ladderLength(beginWord, endWord, wordList):
+        d = defaultdict(list)  # build a graph like this {'h*t': ['hit', 'hot']}
+        for word in wordList:
+            for i in range(len(word)):
+                d[word[:i] + '*' + word[i + 1:]].append(word)
+        q, visited = deque([(beginWord, 1)]), set()
+        while q:
+            word, length = q.popleft()
+            if word == endWord:
+                return length
+            visited.add(word)
+            for i in range(len(word)):
+                for w in d[word[:i] + '*' + word[i + 1:]]:
+                    if w not in visited:
+                        q.append((w, length + 1))
+        return 0
+
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log","cog"]
+
+print(ladderLength(beginWord, endWord, wordList))
